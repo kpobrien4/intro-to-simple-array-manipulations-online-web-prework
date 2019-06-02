735a469e5032070104383f91ff77b199aea8661c
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-02 00:13:18 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+def using_push(countries_in_western_africa, next_country)
+  countries_in_western_africa.push(next_country)
+end
+
+def using_unshift(neighborhoods_in_northwest_brooklyn, @new_neighborhood)
+  neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
+end
+
+def using_pop(@great_hits_of_the_nineties)
+  @great_hits_of_the_nineties.pop
+end
+
+def pop_with_args(@chars_in_game_of_thrones)
+  @chars_in_game_of_thrones.pop(2)
+end
+
+def using_shift(@my_favorite_cities)
+  @my_favorite_cities.shift
+end
+
+def shift_with_args(@ice_cream_brands)
+  @ice_cream_brands.shift(2)
+
+def
