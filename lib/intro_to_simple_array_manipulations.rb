719adca6809752fc9dd0f26713a57f7a56ec0723
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-02 00:18:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_push(countries_in_western_africa, next_country)
   countries_in_western_africa.push(next_country)
 end
 
-def using_unshift(neighborhoods_in_northwest_brooklyn, @new_neighborhood)
-  neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end
