3bc9aa77d17ae1647c55581c4d0d28fbd88456ea
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-02 00:28:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,6 +38,6 @@ def using_flatten(private_colleges_in_newyork)
   private_colleges_in_newyork.flatten
 end
 
-def using_delete(instructors,"Steven")
+def using_delete(instructors)
   instructors.delete
 end
